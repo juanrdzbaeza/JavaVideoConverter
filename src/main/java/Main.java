6d97e6d409dc60c14d784c189eb4c3c516d55162
Juan <juan.rodriguez@edosoft.es>
2024-10-09 15:23:19 +0100

    se carga el binario de ffmpeg y se configura la entrada, salida y el comando a ejecutar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
+import java.io.IOException;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        String ffmpegPath = "src/main/resources/ffmpeg-7.0.2-amd64-static/ffmpeg";
+        String inputFile = "src/main/resources/videos/IMG_6107.MOV";
+        String outputFile = "src/main/resources/videos/IMG_6107.mp4";
+
+        try {
+            Process process = Runtime.getRuntime().exec(
+                    new String[]{ffmpegPath, "-i", inputFile, "input.mov", "-c", "copy", outputFile}
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
 }
