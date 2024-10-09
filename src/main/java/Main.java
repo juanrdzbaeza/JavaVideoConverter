f220f0b2fe18c00ca6b44278f2d48d9c83509792
Juan <juan.rodriguez@edosoft.es>
2024-10-09 15:24:30 +0100

    refactorizacion de la prueba inicial funcionando

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
         String ffmpegPath = "src/main/resources/ffmpeg-7.0.2-amd64-static/ffmpeg";
@@ -10,15 +9,16 @@ public class Main {
         String outputFile = "src/main/resources/videos/IMG_6107.mp4";
 
         try {
-            Process process = Runtime.getRuntime().exec(
-                    new String[]{ffmpegPath, "-i", inputFile, "input.mov", "-c", "copy", outputFile}
-            );
-        } catch (IOException e) {
+            Process process = Runtime.getRuntime().exec(new String[]{ffmpegPath, "-i", inputFile, "-c", "copy", outputFile});
+            int exitValue = process.waitFor();
+            if (exitValue == 0) {
+                System.out.println("Video conversion completed successfully.");
+            } else {
+                System.out.println("Video conversion failed with exit value: " + exitValue);
+            }
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
-
-
     }
 
 }
