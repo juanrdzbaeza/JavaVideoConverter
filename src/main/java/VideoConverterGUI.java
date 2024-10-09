ce3903a787646aba93505850f1b5b6122db84d5a
Juan <juan.rodriguez@edosoft.es>
2024-10-09 15:27:36 +0100

    inicialización de clase para la interfaz de usuario

---
+++ src/main/java/VideoConverterGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VideoConverterGUI extends JFrame implements ActionListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
